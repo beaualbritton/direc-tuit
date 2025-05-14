295a6e755bbe5ef8225a8d25258460a2c6f76753
beaualbritton <albrittoncb@gmail.com>
2025-05-14 13:00:02 -0400

    fullscreen windows, rendering loop.
    ADD: Screen component that takes up the viewports (terminal)  width & height. Basic window toggling. __φ(．．)
    -render() returns a window(), can press q to open and quit dialogue options
    -TODO: fix incessant use of  auto keywords, documentation was ripe with them. need to dig a little deeper.
    -TODO: file input/output. scrolling menu configuration

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,47 @@
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
 #include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+
 using namespace ftxui;
 
-int main() {
-  //FTXUI element: text with a box border
-  auto document = text("Hello, World!") | border;
+int main()
+{
+  //Displaying fullscreen in terminal interface. Takes width/height of current viewport. 
+  //See ftxui/component/screen_interactive.hpp
+  auto screen = ScreenInteractive::Fullscreen();
+  //Renderer component that will be passed to screen.loop()
+  //Within that renderer is a single window (with just text for now)
+  auto verticalContainer = vbox({text("Press 'q' to quit.")}) | border;
+  auto introText = vbox({paragraph("welcome to produc-tuity")});
+  auto openWindow = window(text("open text") | italic, introText);
+  auto infoWindow = window(text("info-mation") | italic, verticalContainer); 
+  auto focusedWindow = openWindow;
+  bool focused = false;
+  Component component = Renderer([&] {
+    if(!focused)
+    {
+      focusedWindow = openWindow;
+    }
+    else
+    {
+      focusedWindow = infoWindow;
+    }
 
-  //Screen with full width and height fitting the document
-  auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(document));
+      return window(
+          text("produc-tuity") | bold,
+          focusedWindow 
+      );
+  });
 
-  // Render the document to the screen buffer and print it
-  Render(screen, document);
-  screen.Print();
+  // Wrap with quit-on-'q' behavior
+  component = CatchEvent(component, [&](Event event) {
+      if (event == Event::Character('q'))
+         focused = !focused; 
+      return false;
+  });
+
+  //Rendering loop, everything within window.
+  screen.Loop(component);
   return 0;
 }
