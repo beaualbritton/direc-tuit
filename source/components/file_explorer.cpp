ce913fb536e5c982f33a3838e0cd9f08640a2d9e
caveblind <albrittoncb@gmail.com>
2025-06-14 01:44:48 -0400

    fix: midnight docs & refactors

--- source/components/file_explorer.cpp
+++ source/components/file_explorer.cpp
@@ -30,6 +30,19 @@ Component fileExplorer() {
 
   populate(fileContainer, currentPath);
 
+  // TODO: ADD OPERATIONS (make, delete, open)
+
+  /*
+   TODO: ADD:
+   * -Sidebar with pinned dirs (Home, Desktop, Documents, etc.)
+   * -Text that displays current directory name
+   * -Use existing directory or make new? (new function/popup)
+   * -pointerSelect button
+   *  Input box for typing
+   *  Keybinds for file operations (create, delete, open.)
+   *  Some more robust error handling (if directory shit is weird)
+   */
+
   Component explorer = Renderer(fileContainer, [=] {
     return window(text("file explorer"),
                   vbox({
@@ -41,12 +54,13 @@ Component fileExplorer() {
   });
   return explorer;
 }
-
+// TODO: instead of populate populating THE ENTIRE container, just have a nested
+// container or vector (for separate tabs/sidebars that will be added)
 void populate(shared_ptr<ComponentBase> pContainer, const path &pPath) {
   pContainer->DetachAllChildren();
 
   Component parentDirButton = Button("../", [pPath, pContainer] {
-    if (pPath.has_parent_path()) {
+    if (pPath.has_parent_path() && is_directory(pPath.parent_path())) {
       populate(pContainer, pPath.parent_path());
     }
   });
@@ -54,11 +68,12 @@ void populate(shared_ptr<ComponentBase> pContainer, const path &pPath) {
   shared_ptr<string> sharedString = make_shared<string>();
   for (auto const &entry : std::filesystem::directory_iterator{pPath}) {
     const path iterPath = entry.path();
-    string label;
+    string label = "";
     if (is_directory(iterPath))
-      label = ": " + iterPath.filename().string();
+      label += ": "; //+ iterPath.filename().string();
     else
-      label = ": " + iterPath.filename().string();
+      label += ": ";
+    label += iterPath.filename().string();
     *sharedString = label;
     Component fileButton = Button(*sharedString, [iterPath, pContainer] {
       if (is_directory(iterPath)) {
