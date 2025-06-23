d3c99cedb68fb09e43674ca188fe790734f754ba
caveblind <albrittoncb@gmail.com>
2025-06-23 17:26:28 -0400

    feat: horizontal popup window
    simple popup window rendering
    todo:
    -add std::function<void> onConfirm, onCancel to parameters of horizontalPopup (allows for passing of lambdas?) hard maybe
    -add boolean to track if actions prior to func call need to be completed
    -implement vbox popup (going to be a menu for file operations)

---
+++ source/components/popup.cpp
@@ -0,0 +1,23 @@
+#include <filesystem>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+using namespace ftxui;
+
+Component horizontalPopup(std::string message, bool *modalFlag) {
+  Component confirmButton, cancelButton;
+  confirmButton = Button(
+      "[Y]es", [modalFlag] { *modalFlag = false; }, ButtonOption::Ascii());
+  cancelButton = Button(
+      "[N]o", [modalFlag] { *modalFlag = false; }, ButtonOption::Ascii());
+  Component popupBody = Container::Horizontal({confirmButton, cancelButton});
+  Component popup = Renderer(popupBody, [popupBody, message] {
+    return window(text(message), popupBody->Render()) |
+           size(WIDTH, GREATER_THAN, 30) | size(HEIGHT, GREATER_THAN, 5) |
+           border | center;
+  });
+
+  return popup;
+}
---
+++ source/components/popup.hpp
@@ -0,0 +1,9 @@
+#include <filesystem>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+using namespace ftxui;
+
+Component horizontalPopup(std::string msg, bool *modalFlag);
+Component verticalPopUp();
