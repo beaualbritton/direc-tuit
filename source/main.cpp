01a7244cb9ede10c0facd970dbe65217ce7c905b
caveblind <albrittoncb@gmail.com>
2025-07-12 20:13:55 -0400

    small fix: cleaning up main and adding light comments

--- source/main.cpp
+++ source/main.cpp
@@ -4,19 +4,17 @@
 #include <ftxui/component/component_base.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
-#include <iostream>
 using namespace ftxui;
 
 int main(int argc, char *argv[]) {
-  // testing user_config stuff
-  std::cout << "checkConfig returns: " << checkConfig();
-
   // Displaying fullscreen in terminal interface. Takes width/height of current
   // viewport. See ftxui/component/screen_interactive.hpp
   //
   if (argc > 1)
     WindowRender::instance().setupWindow(argv[1]);
   else
+    // Passing empty arg to setupWindow() which will always get currentPath
+    // unless argv[1] path exists
     WindowRender::instance().setupWindow("");
 
   // Renderer component that will be passed to screen.loop()
